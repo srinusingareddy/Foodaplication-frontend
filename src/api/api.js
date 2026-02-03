a58b6638970ffeee04d4ce9879a11ccb3b477d2d
SRINUSINGAREDY <srinusingareddy432@gmail.com>
2026-02-03 10:23:00 +0530

    frontend deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,22 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8080/api",
+//   headers: { "Content-Type": "application/json" },
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: { "Content-Type": "application/json" },
 });
 
